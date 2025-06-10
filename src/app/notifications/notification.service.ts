d01218b3b6e8c003e009e8ba20df31bc80aa3093
JimenoDS <jajimenez.2@sudamericano.edu.ec>
2025-06-10 00:05:16 -0500

    csmbio final

--- src/app/notifications/notification.service.ts
+++ src/app/notifications/notification.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NotificationService {
-  private apiUrl = 'http://localhost:3000/notifications';
+  private apiUrl = 'https://debernotificaciones-production.up.railway.app/';
 
   constructor(private http: HttpClient) {}
 
